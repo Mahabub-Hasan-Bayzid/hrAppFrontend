65b91e7d00afaab1cf66a079ab1eeed6e5d09e05
Mahabub-Hasan-Bayzid <s2500143@edu.bc.fi>
2025-06-16 14:44:41 +0300

    BUILD

--- src/hrinfo/Employeecard.jsx
+++ src/hrinfo/Employeecard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
-import styles from "./EmployeeCard.module.css";
+// import styles from "./EmployeeCard.module.css";
 
 const EmployeeCard = ({
   id,
